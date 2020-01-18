7ebf19689df28ee21e170126fb88f56ee6fc2448
Jared Harper <har18082@byui.edu>
2020-01-18 02:59:41 -0500

    almost done

---
+++ web/Assignments.php
@@ -0,0 +1,67 @@
+<?php  
+
+$dirs = array_filter(glob('*'), 'is_dir');
+print_r( $dirs);
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="theStyle.css">
+    <title>Assignments</title>
+</head>
+<body>
+    <header><h1>Mysite</h1><nav><ul id="top_menu"><li>Games</li> <li>Email</li> <li>Forums</li><li>Stories</li><li>Music</li><li>Technology</li><li>News</li></ul></nav><div id="the_date"><h4>The Current Time</h4> <span><?php echo date("l m d Y h:i:sa")  ?></span></div></header>
+
+    <div id="contain">
+    <section id="sidebar_1">
+        <ul>
+            <?php  ?>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li>item 1 </li>
+        </ul>
+    </section>
+    <Article id="MainStuff">
+
+    This Page Contains the link to the Assignments I will be doing for this class
+
+
+
+
+
+    </Article>
+    <section id="sidebar_2">
+    <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+        <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+        <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+    </section></div>
+    <footer>
+        <ul id="bottom_menu">
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li>item 1 </li>
+        </ul>
+    </footer>
+
+
+
+    
+</body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="theStyle.css">
+    <title>MyHome</title>
+</head>
+<body>
+    <header><h1>Mysite</h1><nav><ul id="top_menu"><li>Games</li> <li>Email</li> <li>Forums</li><li>Stories</li><li>Music</li><li>Technology</li><li>News</li></ul></nav></header>
 
-  phpinfo();
+    <div id="contain">
+    <section id="sidebar_1">
+        <ul>
+            <li> <a href="Assignments.php">Assigments</a>  </li>
+            <li> <a href="stats.php">Php stats</a>  </li>
+            <li> Item 1 </li>
+            <li> Item 1 </li>
+            <li> Item 1 </li>
+            <li> Item 1 </li>
+            <li>Item 1 </li>
+        </ul>
+    </section>
+    <Article id="MainStuff">
 
-?>
+   <div><h3>Robots</h3><p>Mechanical marvels that con do so much while taking your job</p></div>
+   <div><h3>Robots</h3><p>Mechanical marvels that con do so much while taking your job</p></div>
+   <div><h3>Robots</h3><p>Mechanical marvels that con do so much while taking your job</p></div>
+   <div><h3>Robots</h3><p>Mechanical marvels that con do so much while taking your job</p></div>
+   
+
+
+
+
+
+    </Article>
+    <section id="sidebar_2">
+        <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+        <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+        <div>  the iron driver notices the steel maiden as they go on and wreak havok</div>
+    </section></div>
+    <footer>
+        <ul id="bottom_menu">
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li> item 1 </li>
+            <li>item 1 </li>
+        </ul>
+    </footer>
+
+
+
+    
+</body>
+</html>
